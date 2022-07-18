ae83d41cc9cd17c54e0a976f25d86a03a7ac37b3
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-07-18 14:55:17 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,5 @@
-test('', () => {});
+test('Se a aplicação é redirecionada para a página de About', () => {});
+
+test('Se a aplicação é redirecionada para a página de Pokémons Favoritados', () => {});
+
+test('Se a aplicação é redirecionada para a página Not Found', () => {});
