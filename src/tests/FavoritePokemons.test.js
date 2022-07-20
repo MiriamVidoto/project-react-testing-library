27d000dc37c14b4d83573ec8eed1eb39197784e2
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-07-19 21:03:57 -0300

    Requisito 03 ok

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './helper/renderWithRouter';
+
+describe('Teste do componente <NotFound.js />', () => {
+  test('Se é exibida na tela uma mensagem, caso não tenha pokémons favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+
+    const text = screen.getByText(/No favorite pokemon found/i);
+    expect(text).toBeDefined();
+  });
+
+  test('Se são exibidos todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const details = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(details);
+
+    const checked = screen.getByRole('checkbox', { name: /Pokémon favoritado?/i });
+    userEvent.click(checked);
+
+    history.push('/favorites');
+
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    expect(moreDetails).toBeDefined();
+  });
+});
